f3ef0eb513bb7efc8967b7c9826752569c35a12c
Nullptr <52071314+Dr-TSNG@users.noreply.github.com>
2022-02-08 23:05:11 +0800

    Bump with 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ fun String.execute(currentWorkingDir: File = file("./")): String {
 }
 
 val verCode by extra("git rev-list HEAD --count".execute().toInt())
-var verName by extra("0.8")
+var verName by extra("2.0")
 
 val minSdkVer by extra(23)
 val targetSdkVer by extra(32)
